7fdd9021266b5b1804dc52bdc5b144d19c0276ea
Dan Skorupski <dan.skorupski@gmail.com>
2017-01-19 10:53:41 -0600

    Simplify

--- build.py
+++ build.py
@@ -3,35 +3,29 @@ from urllib.request import urlopen
 from urllib.parse import urlencode
 import csv
 import datetime
+import io
+import json
 import os
 import os.path
-import time
-import json
 import string
+import time
 
 def fetch(sym, begin_date, end_date, *, dividends):
-    path = sym.lower() + "-"
-    path += "d" if dividends else "p"
-    path += ".csv"
+    query = {
+        "s": sym.upper(),
+        "a": begin_date.month - 1,
+        "b": begin_date.day,
+        "c": begin_date.year,
+        "d": end_date.month - 1,
+        "e": end_date.day,
+        "f": end_date.year,
+    }
 
-    if not os.access(path, os.R_OK) or os.stat(path).st_mtime < time.time() - 12*60*60:
-        with open(path, "wb") as outf:
-            query = {
-                "s": sym.upper(),
-                "a": begin_date.month - 1,
-                "b": begin_date.day,
-                "c": begin_date.year,
-                "d": end_date.month - 1,
-                "e": end_date.day,
-                "f": end_date.year,
-            }
-            if dividends:
-                query["g"] = "v"
-            with urlopen("http://ichart.finance.yahoo.com/table.csv?" + urlencode(query)) as inf:
-                outf.write(inf.read())
+    if dividends:
+        query["g"] = "v"
 
-    with open(path) as f:
-        result = [row for row in csv.DictReader(f)]
+    with urlopen("http://ichart.finance.yahoo.com/table.csv?" + urlencode(query)) as f:
+        result = [row for row in csv.DictReader(io.TextIOWrapper(f))]
 
     result.reverse() # return ascending, not descending
     return result
@@ -46,11 +40,12 @@ def main():
     begin_date = datetime.date(2017, 1, 17)
     end_date = datetime.date(2017, 12, 29)
     repls = {
-        "ntdoy_p": json.dumps(fetch("ntdoy", begin_date, end_date, dividends=False)),
-        "ntdoy_d": json.dumps(fetch("ntdoy", begin_date, end_date, dividends=True)),
-        "fds_p": json.dumps(fetch("fds", begin_date, end_date, dividends=False)),
-        "fds_d": json.dumps(fetch("fds", begin_date, end_date, dividends=True))
+        "ntdoy_p": fetch("ntdoy", begin_date, end_date, dividends=False),
+        "ntdoy_d": fetch("ntdoy", begin_date, end_date, dividends=True),
+        "fds_p": fetch("fds", begin_date, end_date, dividends=False),
+        "fds_d": fetch("fds", begin_date, end_date, dividends=True)
     }
+    repls = {k: json.dumps(v) for k, v in repls.items()}
 
     with open(os.path.expanduser("~/public_html/stockfite.html"), "w") as outf:
         outf.write(templ.substitute(repls))
